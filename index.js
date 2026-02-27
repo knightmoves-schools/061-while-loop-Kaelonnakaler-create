f413d8013c5f634ef675c9811a1cc401e9ccc120
kaelonnakaler-create <kaelonna.kaler@students.msswarriors.org>
2026-02-27 14:41:45 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 function markAsDone(todos){
   let i = 0;
-  while(i < 1 todos.length) {
-    todos[i] = "done -" + todos [i];
+  while(i < todos.length) {
+    todos[i] = "done - " + todos [i];
     i++;
   }
   return todos;
 }
+const tasks = ["washes dishes", "clean room", "switch laundry"];
+
 
 
